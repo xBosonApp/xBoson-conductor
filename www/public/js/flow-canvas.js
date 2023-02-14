852a21f300bc630d5a804c255a08b144c8bcd9ec
yanming <yanming-sohu@sohu.com>
2023-02-14 09:54:42 +0800

    设置前端文件版权信息

--- www/public/js/flow-canvas.js
+++ www/public/js/flow-canvas.js
@@ -1,3 +1,18 @@
+/**
+ *  Copyright 2023 Jing Yanming
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 //
 // 这个脚本是整个 EEB-UI 的核心, 同时负责与 Core 通讯
 //
